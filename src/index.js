66fb7236f872d4fb525241a88af27619553e87d4
Ekaterina <ekaterina.yurkevich@gmail.com>
2021-08-23 16:47:19 +0300

    init: change file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let array = [];
+  let i = Math.abs(n);
+  while (i >= 1) {
+      array.unshift(i % 10);
+      i = Math.trunc(i / 10);
+  }
+  array.reverse();
+  let result = 0;
+  for (let j = 0; j < array.length; j++) {
+      result = result * 10 + array[j];
+  }
+  return result;
 }
